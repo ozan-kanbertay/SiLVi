0a23a6ed9d74f06e6d005a85feba8ee6f9b0c87d
Ozan Kanbertay <81885929+ozan-kanbertay@users.noreply.github.com>
2026-01-26 05:30:24 +0100

    Add flatpak dependencies to build.yml

--- forge.config.js
+++ forge.config.js
@@ -55,9 +55,10 @@ module.exports = {
           categories: ['Science', 'Video'],
           icon: 'icons/icon.png',
           name: 'SILVI',
-          // id: 'org.flatpak.silvi',
+          runtimeVersion: '25.08',
+          baseVersion: '25.08',
+          id: 'org.flatpak.silvi',
           description: 'SILVI enables researchers to annotate behaviors and interactions directly within video data, generating structured outputs suitable for training and validating computer vision models.',
-          // base: 'org.flatpak.silvi.BaseApp'
         }
       }
     },
